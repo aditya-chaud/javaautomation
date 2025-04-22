8ca224b03746eec5bd7445b6084e2aabe316c398
aditya-chaud <69751464+aditya-chaud@users.noreply.github.com>
2025-04-22 07:31:40 +0545

    Check (#8)
    * alerttest
    
    * testalert

--- src/test/java/CheckboxnAlerts/Alert.java
+++ src/test/java/CheckboxnAlerts/Alert.java
@@ -3,5 +3,6 @@ package CheckboxnAlerts;
 public class Alert {
     public static void main(String[] args){
         System.out.println("this is alert");
+        System.out.println("alert again");
     }
 }
