924bb9af6428caf0f771cb5d6347baa427819d0f
aditya-chaud <69751464+aditya-chaud@users.noreply.github.com>
2025-05-27 21:30:23 +0545

    automation
    * frames
    
    * edited newlines
    
    * testng
    
    * assertions
    
    * grouping_dependency
    
    * dataproviders-datadriventesting
    
    * POM and listeners
    
    * autm

---
+++ src/test/java/AutoFramew/Automationf.java
@@ -0,0 +1,37 @@
+package AutoFramew;
+
+public class Automationf {
+}
+
+/*Automation Framework is the structure that we organize automation project files/folders in a structured manner.
+Objectives:
+Reusability
+Maintainability
+Readability
+
+Types of frameworks:
+built-in:testng, cucumber, junit, robot
+customized: modular, data driven, keyword driven, hybrid
+
+Stages of Aut framework:
+1. Analysing Application under test
+   Number of pages
+   elements
+   what to automate/what not to automate
+2. Categorize and choose the test cases.
+Sanity(p1): Basic functionality test cases(Must be given highest priority to automate.
+Data Driven(p2): must automate
+Regression(p3): must automate impact test cases and main features.
+Any other test cases(4): may or may not
+
+3. Design and development of the framework
+Blueprint of the framework. like creating folder, files structure.
+
+4. Execution
+Local: in your ide
+Remote: like in jenkins which is devops/CI tool which will automatically pull those test cases from github repos and execute in their env
+
+5. Maintenance
+Repositories.
+
+*/
